a92fd5313b4e379ad092d5609a9b15c63df6c7ee
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-24 23:40:33 -0500

    add phone nav toggle function

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,9 @@
-const hello = "hello world";
+const burger = document.querySelector(".burger");
+const links = document.querySelector("#links");
+
+const toggleLinks = () => {
+  links.classList.toggle("active");
+  burger.classList.toggle("toggle");
+};
+
+burger.addEventListener("click", toggleLinks);
