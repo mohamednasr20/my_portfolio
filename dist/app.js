b8bb6ed692c4296be23d911b224b933cca6c5ebe
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-25 23:08:20 -0500

    add nav background on scroll

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,17 @@
 const burger = document.querySelector(".burger");
 const links = document.querySelector("#links");
+const nav = document.querySelector("#main-nav");
 
 const toggleLinks = () => {
   links.classList.toggle("active");
   burger.classList.toggle("toggle");
 };
 
+const handleNavbarBackground = () => {
+  document.body.scrollTop > 50 || document.documentElement.scrollTop > 50
+    ? nav.classList.add("navbar-bg")
+    : nav.classList.remove("navbar-bg");
+};
+
 burger.addEventListener("click", toggleLinks);
+window.addEventListener("scroll", handleNavbarBackground);
