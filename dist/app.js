ea1e92448484772e0476b3e226fc78459a8b8c27
mohamednasr20 <mohamednasr8687@gmail.com>
2021-03-01 22:48:14 -0500

    change color scheme

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,17 @@
-const burger = document.querySelector(".burger");
-const links = document.querySelector("#links");
-const nav = document.querySelector("#main-nav");
+const burger = document.querySelector('.burger');
+const links = document.querySelector('#links');
+const nav = document.querySelector('#main-nav');
 
-const toggleLinks = () => {
-  links.classList.toggle("active");
-  burger.classList.toggle("toggle");
+const handletoggleNavLinks = () => {
+  links.classList.toggle('active');
+  burger.classList.toggle('toggle');
 };
 
 const handleNavbarBackground = () => {
   document.body.scrollTop > 50 || document.documentElement.scrollTop > 50
-    ? nav.classList.add("navbar-bg")
-    : nav.classList.remove("navbar-bg");
+    ? nav.classList.add('navbar-bg')
+    : nav.classList.remove('navbar-bg');
 };
 
-burger.addEventListener("click", toggleLinks);
-window.addEventListener("scroll", handleNavbarBackground);
+burger.addEventListener('click', handletoggleNavLinks);
+window.addEventListener('scroll', handleNavbarBackground);
